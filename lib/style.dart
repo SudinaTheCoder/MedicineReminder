3c95523bd21caa707ebe66962f3bf2a4ba2db948
Sudina Shrestha <sudinashrestha111@gmail.com>
2021-12-28 14:23:08 +0545

    First push

---
+++ lib/style.dart
@@ -0,0 +1,48 @@
+import'package:flutter/material.dart';
+ const greetingTitleStyle=TextStyle(
+       fontSize: 36,
+       fontWeight: FontWeight.w500,
+       color:Colors.white,
+     );
+
+       const greetingSubtitleStyle=TextStyle(
+       fontSize: 20,
+       fontWeight: FontWeight.w300,
+       color:Colors.white,
+      );
+      const notificationTitleStyle = TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        color:Colors.white,
+      );
+      const notificationButtonStle = TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w300,
+        color: Colors.white,
+      );
+      const nextAppointmentTitleStyle = TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.w500,
+        color: Colors.black,
+      );
+       const nextAppointmentSubtitleStyle = TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        color: Colors.white54,
+      );
+       const nextAppointmentMainStyle = TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w600,
+        color: Colors.black,
+        height: 1.5,
+      );
+        const appointmentDateStyle = TextStyle(
+          color: Colors.black45,
+          fontWeight: FontWeight.w400,
+          fontSize: 16,
+      );
+        const appointmentvenueStyle = TextStyle(
+          color: Colors.black38,
+          fontWeight: FontWeight.w400,
+          fontSize: 15,
+      );
